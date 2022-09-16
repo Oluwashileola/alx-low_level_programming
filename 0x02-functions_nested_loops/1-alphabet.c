eace0c0068bcad991c73ae9338a484baa0d2bfdd
Akindoju Shileola <spickbliss@gmail.com>
2022-09-16 03:33:30 -0700

    added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lower case
+ * followed by a new line
+ */
+
+void print_alphabet(void);
+{
+		char letter;
+		letter = 'a'
+
+		while (letter <= 'z')
+			_putchar(letter);
+
+		letter++;
+		_putchar('\n')
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - checks the 1-alphabet.c code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+		print_alphabet();
+
+		return (0);
+}
