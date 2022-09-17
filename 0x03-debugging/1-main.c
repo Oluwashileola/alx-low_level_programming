300aca67d9a940499a77eab8eaaf3bf67643b95f
Akindoju Shileola <spickbliss@gmail.com>
2022-09-17 12:02:47 -0700

    corrected 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,16 +1,29 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
-  * main - Test function for positive or negative
-  * Return: 0
-  */
+ * main - Causes an infinite loop
+ *
+ * Return: 0
+ *
+ */
+
+
 
 int main(void)
+
 {
 		int i;
 
+		printf("Infinite loop incoming :(\n");
+
 		i = 0;
-		positive_or_negative(i);
+
+		while (i < 10)
+		{
+			putchar(i);
+		}
+
+		printf("Infinite loop avoided! \\o/\n");
 
 		return (0);
 }
