117c8a52a6492dffa1cce7a960ecc830e82b99c2
Akindoju Shileola <spickbliss@gmail.com>
2022-09-17 07:31:40 -0700

    updated source files in 0x02 directory tocomply with betty style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,15 @@
 
 /**
  * _islower - Checks if a character is lowercase.
- * c: The character to be checked.
+ * @c: The character to be checked.
  *
  * Return: 1 if character is lowercase and 0 if otherwise.
  */
 
 int _islower(int c)
 {
-    if (c >= 'a' && c <= 'z')
-		  return (1);
-	  else
-		  return (0);
+		if (c >= 'a' && c <= 'z')
+			return (1);
+		else
+			return (0);
 }
