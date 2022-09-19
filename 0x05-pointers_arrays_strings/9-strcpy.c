9cbe4a138b9fefb33a12b3a12732062bf406d695
Akindoju Shileola <spickbliss@gmail.com>
2022-09-19 07:49:21 -0700

    updated source codes 4 and 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,12 +2,13 @@
 
 /**
  * _strcpy - Copies a string pointed to by @src, including the
- *           terminating null byte, to a buffer pointed to by @dest.
+ * terminating null byte, to a buffer pointed to by @dest.
  * @dest: A buffer to copy the string to.
  * @src: The source string to copy.
  *
  * Return: A pointer to the destination string @dest.
  */
+
 char *_strcpy(char *dest, const char *src)
 {
 	int index = 0;
