e5fb48addcf56f0b8297d3fcf3b86a9e2e64aeb2
Oluwashileola <111228750+Oluwashileola@users.noreply.github.com>
2022-12-13 07:55:38 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,18 @@
-#include "main.h"
-
-/**
- * _strlen - calculate the length of a string.
- *
- * @s: the string to calculate it's length.
- * Return: length of a string.
- */
-
-int _strlen(char *s)
-{
-	int i;
-	int length = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		length++;
-	}
-	return (length);
+#include "main.h"                                                                                                                       
+                                                                                                                                        
+/**                                                                                                                                     
+ * _strlen - Returns the length of a string.                                                                                            
+ * @str: The string to get the length of.                                                                                               
+ *                                                                                                                                      
+ * Return: The length of @str.                                                                                                          
+ */                                                                                                                                     
+                                                                                                                                        
+size_t _strlen(const char *str)                                                                                                         
+{                                                                                                                                       
+        size_t length = 0;                                                                                                              
+                                                                                                                                        
+        while (*str++)                                                                                                                  
+                length++;                                                                                                               
+                                                                                                                                        
+        return (length);                                                                                                                
 }
